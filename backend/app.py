51f7e65427fc7a6ce8aed76a0c31a4b69cc20e55
ggxxding <ggxxding@163.com>
2022-12-09 16:30:33 +0800

    test2

--- backend/app.py
+++ backend/app.py
@@ -108,7 +108,6 @@ def loadWeiboData():
 				'2020': [0 for i in range(12)],
 				'earlier': [0 for i in range(12)],
 			}
-			
 			timeList[label] =  temp
 			cityList[label] = {'24h':[],'30d':[], '90d':[], '365d':[]}
 		# now = datetime.datetime.now()..
